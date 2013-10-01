968add823d46da94efbdd5352d1d6acdea704683
Francois Michonneau <francois.michonneau@gmail.com>
2013-10-01 16:08:11 -0400

    improved M&M paragraph about path sampling, started on GMYC analyses

--- code/impatiens_analysis.R
+++ code/impatiens_analysis.R
@@ -28,21 +28,24 @@ runNJonAlg <- function(folder, pattern, db=impDB) {
   setwd(owd)
 }
 
-makeTraitFile <- function(alg, output, db=impDB) {
+makeTraitFile <- function(alg, output, db=impDB, allowedESUs,
+                          format="fasta", ...) {
 ### generate the traits file to be used by *BEAST
-### alg -- path to alignment in PHYLIP format
+### alg -- path to alignment in PHYLIP/FASTA format
 ### output -- path and filename of where to write the output
+### allowedESUs -- safe guard to validate that the ESU in database match
+####   what is intended.
+### ... -- additional arguments to be passed to ape:::read.dna
   if (!file.exists(alg)) stop(alg, "doesn't exist")
-  algF <- read.dna(file=alg)
+  algF <- read.dna(file=alg, format=format, ...)
   tmpRes <- cbind(traits=db$Extract, species=db$consensusESU)
   tmpRes <- tmpRes[nzchar(tmpRes[,1]), ]
   tmpRes[,1] <- gsub(",.+$", "", tmpRes[,1]) # replace extractions by first one if multiple to follow mergeSeq rule
   tmpRes <- subset(tmpRes, tmpRes[,1] %in% dimnames(algF)[[1]])
-  allowedESUs <- c("ESU1", "ESU2", "ESU3", "tiger", "RedSea", "Hawaii", "WA", "EP", "Wpac", "gracilis", "Gala", "tigerRedSea", "Medit")
   notAllowed <- tmpRes[,2][! tmpRes[,2] %in% allowedESUs]
   if (length(notAllowed) > 0) warning(paste(unique(notAllowed), collapse=","), ", not found in DB")
   if (any(is.na(tmpRes[,2]))) warning("some NAs found in DB")
-  if (length(!nzchar(tmpRes[,2]))) warning("some ESUs not specified")
+  if (any(!nzchar(tmpRes[,2]))) warning("some ESUs not specified")
   write.table(tmpRes, file=output, row.names=FALSE, quote=FALSE, sep="\t")
   TRUE
 }
@@ -51,13 +54,10 @@ impDB <- read.csv(file="~/Documents/Impatiens/impatiens_phylogeography/data/impa
 impExt <- impDB$Extract[nzchar(impDB$Extract)]
 stopifnot(ncol(impDB) > 1)
 
-impESU1 <- subset(impDB, molecularESU == "ESU1" | molecularESU == "Hawaii")
-impESU1 <- impESU1$Extract[nzchar(impESU1$Extract)]
-
 ### Main analysis -- these commands should produce the final output for the published
 ###  analyses. If it's not the case, it should go somewhere else.
 
-## Create alignments for individual markers
+## Create alignments for individual markers for main phylogeny
 impAlg <- mergeSeq(impExt, output="/tmp/seq", seqFolder="~/Documents/seqRepository",
                    markers=c("16S", "16Sc", "COI", "ATP6", "c0036", "c0775", "ITS", "LSU", "H3a"),
                    gblocks=list("ITS" = "-t=d -b4=5 -b5=a -p=n"), justCheck=F)
@@ -68,18 +68,49 @@ alg2nex(file="/tmp/seq/20130923.impatiens.phy", partition.file="/tmp/seq/2013092
 
 ## unfortunately at this stage, still need manual editing of the partition... compare -orig.nex and .nex
 
-## fasToPhase("~/Documents/Impatiens/000.currentFiles/20130710-224540-c0036.afa") # removed N1167 by hand (too much missing data)
-## fasToPhase("~/Documents/Impatiens/000.currentFiles/20130710-224540-c0775.afa")
-## fasToPhase("~/Documents/Impatiens/000.currentFiles/20130710-224540-H3a.afa")
-## fasToPhase("~/Documents/Impatiens/000.currentFiles/20130710-224540-ITS.afa")
-## fasToPhase("~/Documents/Impatiens/000.currentFiles/20130710-224540-LSU.afa")
-## setwd("../20130711.phase")
+## Create alignements for individual markers for *BEAST analysis of ESU1 "complex"
+##   For this, first look at available sequences for the complex and select specimens
+##   that have at least 1 mt marker and 1 nuc marker (except for LSU that is too conserved).
 
-## system("./PHASE -d1 20130710-224540-c0036.inp c0036.out")
-## system("./PHASE -d1 20130710-224540-c0775.inp c0775.out")
-## system("./PHASE -d1 20130710-224540-H3a.inp H3a.out")
-## system("./PHASE -d1 20130710-224540-ITS.inp ITS.out")
-## system("./PHASE -d1 20130710-224540-LSU.inp LSU.out")
+impExtESU1tmp <- subset(impDB, consensusESU %in% c("ESU1", "ESU3", "gracilis", "Hawaii", "Wpac", "RedSea"))
+impExtESU1tmp <- impExtESU1tmp$Extract[nzchar(impExtESU1tmp$Extract)]
+impAlgESU1tmp <- mergeSeq(impExtESU1tmp, output="/tmp/seq", seqFolder="~/Documents/seqRepository",
+                          markers=c("16S", "16Sc", "COI", "ATP6", "c0036", "c0775", "ITS", "LSU", "H3a"),
+                          justCheck=TRUE)
+impExtESU1 <- rownames(subset(impAlgESU1tmp, (impAlgESU1tmp$"16S" == TRUE | impAlgESU1tmp$"16Sc" == TRUE |
+                                              impAlgESU1tmp$"COI" == TRUE | impAlgESU1tmp$"ATP6" == TRUE) &
+                              (impAlgESU1tmp$"c0036" == TRUE | impAlgESU1tmp$"c0775" == TRUE |
+                               impAlgESU1tmp$"ITS" == TRUE | impAlgESU1tmp$"H3a" == TRUE)))
+impAlgESU1 <- mergeSeq(impExtESU1, output="/tmp/seq", seqFolder="~/Documents/seqRepository",
+                       markers=c("16S", "16Sc", "COI", "ATP6", "c0036", "c0775", "ITS", "LSU", "H3a"),
+                       gblocks=list("ITS" = "-t=d -b4=5 -b5=a -p=n"), justCheck=F)
+concatenateAlignments(pattern="afa$", path="~/Documents/Impatiens/20130929.impatiens_sampling",
+                      output="~/Documents/Impatiens/20130929.impatiens_sampling/20130930.impatiens_starbeast.phy",
+                      partition="~/Documents/Impatiens/20130929.impatiens_sampling/partition",
+                      partition.format="nexus", colw=10000, colsep="")
+alg2nex(file="~/Documents/Impatiens/20130929.impatiens_sampling/20130930.impatiens_starbeast.phy",
+        partition.file="~/Documents/Impatiens/20130929.impatiens_sampling/partition")
+
+makeTraitFile(alg="/tmp/seq/20130930-133004-COI.afa", output="/tmp/seq/traitsAllESUs.txt",
+              allowedESUs=c("ESU1", "ESU3", "RedSea", "Hawaii", "Wpac", "gracilis"))
+
+## fasToPhase("/tmp/seq/20130929-172701-c0036.afa")
+## fasToPhase("/tmp/seq/20130929-172701-c0775.afa")
+## fasToPhase("/tmp/seq/20130929-172701-ITS.afa")
+
+## system("PHASE -d1 /tmp/seq/20130929-172701-c0036.inp /tmp/seq/20130929-172701-c0036.out")
+## system("PHASE -d1 /tmp/seq/20130929-172701-c0775.inp /tmp/seq/20130929-172701-c0775.out")
+## system("PHASE -d1 /tmp/seq/20130929-172701-ITS.inp /tmp/seq/20130929-172701-ITS.out")
+
+## phaseToFas("/tmp/seq/20130929-172701-c0036.out", "/tmp/seq/20130929-172701-c0036.afa",
+##            "/tmp/seq/20130929-172701-c0036-phased.afa", removeEmptySeqs=TRUE)
+
+## phaseToFas("/tmp/seq/20130929-172701-c0775.out", "/tmp/seq/20130929-172701-c0775.afa",
+##            "/tmp/seq/20130929-172701-c0775-phased.afa", removeEmptySeqs=TRUE)
+
+## phaseToFas("/tmp/seq/20130929-172701-ITS.out", "/tmp/seq/20130929-172701-ITS.afa",
+##            "/tmp/seq/20130929-172701-ITS-phased.afa", removeEmptySeqs=TRUE) ## problematic output; only 1 ambiguity
+
 
 ### full impatiens Tree
 ### ---- impatiens-tree ----
@@ -108,6 +139,14 @@ barMonophyletic(groupLabel=esuList, groupMatch=paste("^", esuList, "_", sep=""),
 axis(side=1, at=0:8, labels=paste("-", 8:0, sep=""))
 legend(x=0, y=50, pch=16, col=c("black", "red", "orange"), legend=c("$PP = 1$", "$0.975 \\leq PP < 1$", "$ 0.9 \\leq PP < 0.975$"))
 
+
+### GMYC analyses
+### ---- gmyc-coi ----
+library(splits)
+trBeast <- read.nexus(file="data/20130201-COI-PB-strict.tre.nex")
+trBeast <- drop.tip(trBeast, "S0213")
+simpleGmyc <- gmyc(trBeast)
+multiGmyc <- gmyc(trBeast, method="m", interval=c(-0.2,-0.005))
 
 #####################
 
