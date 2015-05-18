3985aec33c6d7decc2c4648f10da0a2fcbb6c5d5
Francois Michonneau <francois.michonneau@gmail.com>
2015-05-18 19:56:20 -0400

    update install_fonts to use ~/.fonts

--- R/install_fonts.R
+++ R/install_fonts.R
@@ -1,5 +1,5 @@
 install_fonts <- function() {
-    dir.create("~/.texmf-var/luatex-cache/generic/fonts/otf")
-    system("git clone git@github.com:fmichonneau/latex-fonts.git ~/.texmf-var/luatex-cache/generic/fonts/otf")
-    stopifnot(length(list.files(path = "~/.texmf-var/luatex-cache/generic/fonts/otf", pattern = "lua$|luc$")) > 1)
+    dir.create("~/.fonts")
+    system("git clone git@github.com:fmichonneau/latex-fonts.git ~/.fonts")
+    system("fc-cache -f -v")
 }
